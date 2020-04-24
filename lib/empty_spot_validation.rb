8d6c78cdfa07ea5b82014172a8b00f4730a640fc
Henzi <mugunga5@gmail.com>
2020-04-24 22:57:14 +0200

    add lib/validation to updatethe previous commit

--- lib/empty_spot_validation.rb
+++
@@ -1,7 +0,0 @@
-class Empty_spot_validation
-  attr_writer :user_input
-
-  def initialize (user_input)
-    @user_input  = user_input
-  end
-end
---
+++ lib/validation.rb
@@ -0,0 +1,9 @@
+class Validation
+  attr_writer :user_input
+  @user_input
+
+  def user_input
+    @user_input
+  end
+end
+
