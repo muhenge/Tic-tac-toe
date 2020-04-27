bb00947e1129d86ebf4b0227dbcbf3776f2d433c
Henzi <mugunga5@gmail.com>
2020-04-27 17:46:10 +0200

    bug fix- correct rubocop linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -6,30 +6,20 @@ while game_on
   player = count.odd? ? 1 : 2
   puts "Is the turn of the player #{player}. Select one space (1-9)\n\n"
 
-  puts(" 1 | 2 | 3 ")
-  puts("-----------")
-  puts(" 4 | 5 | 6 ")
-  puts("-----------")
-  puts(" 7 | 8 | 9 ")
+  puts(' 1 | 2 | 3 ')
+  puts('-----------')
+  puts(' 4 | 5 | 6 ')
+  puts('-----------')
+  puts(' 7 | 8 | 9 ')
   puts
-  user_input = gets.chomp()
-  
+  user_input = gets.chomp
   count += 1
-
   valid = true
-  puts "invalid move" unless valid
-
+  puts "you choosed the: #{user_input}"
+  puts 'invalid move' unless valid
   winner = true
-  puts "winner player #{player}" if winner 
-
+  puts "winner player #{player}" if winner
   draw = true
-  puts "it's a draw!" if draw
-
-
-  if winner
-    game_on = false
-  end
+  puts 'it\'s a draw!' if draw
+  game_on = false if winner
 end
-
-
-
