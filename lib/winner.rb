8c86e03185760839541399f1adc08e1dd0fd29cb
LOctavio <luis_ram1997@hotmail.com>
2020-04-27 17:22:42 -0500

    add diagonal_winnning_move? method and use uno constant instead two

--- lib/winner.rb
+++ lib/winner.rb
@@ -1,8 +1,7 @@
 
 class Winner
 
-  DIAGONAL_ONE = [1,5,9]
-  DIAGONAL_TWO = [3,5,7]
+  DIAGONAL = [0,4,8,2,4,6]
 
 
   def initialize(arr)
@@ -18,6 +17,7 @@ class Winner
     end
     false
   end
+
   def vertical_winning_move?
     index = 0
     3.times do
@@ -27,22 +27,24 @@ class Winner
     false
   end
 
-  def diagonal_winning_move
-    
+  def diagonal_winning_move?
+    index = 0
+    2.times do
+      return true if (@arr[DIAGONAL[index]] == 'x' && @arr[DIAGONAL[index+1]] == 'x' && @arr[DIAGONAL[index+2]] == 'x') || (@arr[DIAGONAL[index]] == 'o' && @arr[DIAGONAL[index+1]] == 'o' && @arr[DIAGONAL[index+2]] == 'o')
+      index += 3
+    end
+    false
   end
 
   public
 
   def winning_move
-    horizontal_winning_move? ? true : false
-    vertical_winning_move? ? true : false
-    DIAGONAL_ONE
-    DIAGONAL_TWO
+    horizontal_winning_move? or vertical_winning_move? or diagonal_winning_move? ? true : false
   end
 end
 
-win = Winner.new([1,'x',3,4,'x',6,7,'x',9])
-puts win.winning_move
+# win = Winner.new(['o',2,3,'o','o',6,'x',8,'x'])
+# puts win.winning_move
 # [[1,4,7], [2, 5,8], [3, 6, 9]]
 # [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # [[1, 5, 9], [3, 5, 7]]
