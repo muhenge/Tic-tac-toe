f32c1db06c15866e7232fa5f77e3befb6d328038
LOctavio <luis_ram1997@hotmail.com>
2020-04-27 12:03:50 -0500

    add is_valid? method

--- lib/validation.rb
+++ lib/validation.rb
@@ -5,9 +5,20 @@ class Validation
     @user_input = user_input
     @token = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
   end
+
+  public
+
+  def is_valid?
+    (1..9).each do |x|
+      return true if x  == @user_input
+    end
+    false 
+  end
 end
 
-
+instance = Validation.new
+instance.user_input = 10
+instance.is_valid? ? (puts "is valid") : (puts "is not valid")
 
 #arr = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
 #val = 3
