823c63140cc098f19cbc5f7006edcc87ecaaf10f
Henzi <mugunga5@gmail.com>
2020-04-27 18:30:48 +0200

    add initialize method to the validation class and properties

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,9 +1,21 @@
 class Validation
-  attr_writer :user_input
-  @user_input
-
-  def user_input
-    @user_input
+  attr_reader :token
+  attr_accessor :user_input
+  def initialize
+    @user_input = user_input
+    @token = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
   end
 end
 
+
+
+#arr = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
+#val = 3
+#arr[val-1]
+#ask to the player for a choice
+#with the choice
+#-validate
+#--check the spaces to see if is empty or not
+#---put the symbols
+#winner
+#draw
