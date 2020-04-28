26d1d93771b6df93f893eaa01912336a9821da66
Luis Octavio <luis_ram1997@hotmail.com>
2020-04-28 12:23:56 -0500

    add condition to decide what happens when the game is over

--- bin/main.rb
+++ bin/main.rb
@@ -2,40 +2,61 @@
 require '../lib/validation.rb'
 require '../lib/winner.rb'
 require '../lib/draw.rb'
-count = 1
-game_on = true
-valid = Validation.new
-win = Winner.new
-draw = Draw.new
-while game_on
-  player = count.odd? ? 1 : 2
-  puts "Is the turn of the player #{player}\n"
-  puts "Please, select a number to play from 1 to 9\n\n"
-  puts(' 1 | 2 | 3 ')
-  puts('-----------')
-  puts(' 4 | 5 | 6 ')
-  puts('-----------')
-  puts(' 7 | 8 | 9 ')
-  puts
+catch :game do
   loop do
-    user_input = gets.chomp
-    valid.user_input = user_input.to_i
-    if valid.is_valid?
-      if valid.is_avaliable?
-        puts "you choosed the: #{user_input}"
-        valid.add_symbol(count)
-        break
-      else
-        puts 'is not avaliable'
+    count = 1
+    game_on = true
+    valid = Validation.new
+    win = Winner.new
+    draw = Draw.new
+    while game_on
+      player = count.odd? ? 1 : 2
+      puts "Is the turn of the player #{player}\n"
+      puts "Please, select a number to play from 1 to 9\n\n"
+      puts(" #{valid.token[0]} | #{valid.token[1]} | #{valid.token[2]} ")
+      puts('-----------')
+      puts(" #{valid.token[3]} | #{valid.token[4]} | #{valid.token[5]} ")
+      puts('-----------')
+      puts(" #{valid.token[6]} | #{valid.token[7]} | #{valid.token[8]} ")
+      puts
+      loop do
+        user_input = gets.chomp
+        valid.user_input = user_input.to_i
+        if valid.is_valid?
+          if valid.is_avaliable?
+            puts "you choosed the: #{user_input}"
+            valid.add_symbol(count)
+            break
+          else
+            puts 'is not avaliable'
+          end
+        else
+          puts 'is not valid'
+        end
       end
-    else
-      puts 'is not valid'
+      win.arr = valid.token
+      count += 1
+      draw.count = count
+      puts "winner player #{player}" if win.winning_move
+      puts 'it\'s a draw!' if draw.is_draw?
+      game_on = false if win.winning_move or draw.is_draw?
+    end
+    puts
+    puts(" #{valid.token[0]} | #{valid.token[1]} | #{valid.token[2]} ")
+    puts('-----------')
+    puts(" #{valid.token[3]} | #{valid.token[4]} | #{valid.token[5]} ")
+    puts('-----------')
+    puts(" #{valid.token[6]} | #{valid.token[7]} | #{valid.token[8]} ")
+    puts
+    puts "Do you want to play again? (yes, no)"
+    loop do
+      play_again = gets.chomp
+      case play_again
+      when 'no' then throw :game
+      when 'yes' then puts "restarting game\n\n"
+      else puts 'choice a valid word'
+      end
+      break if play_again == 'yes' || play_again == 'no'
     end
   end
-  win.arr = valid.token
-  count += 1
-  draw.count = count
-  puts "winner player #{player}" if win.winning_move
-  puts 'it\'s a draw!' if draw.is_draw?
-  game_on = false if win.winning_move or draw.is_draw?
 end
