68dac6757fa926ad9150111a0f404c1b9d7284ed
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-01 10:16:38 -0500

    delete repeated instance

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,8 +1,8 @@
 require './lib/validation.rb'
 
 describe Validation do
+  let(:validation) { Validation.new }
   describe '#valid?' do
-    let(:validation) { Validation.new }
     it 'the input is not a number between 1 and 9' do
       validation.user_input = 'h'
       expect(validation.valid?).to eql(false)
@@ -15,7 +15,6 @@ describe Validation do
   end
 
   describe '#avaliable?' do
-    let(:validation) { Validation.new }
     it 'the space is not avaliable to choose' do
       validation.user_input = 3
       validation.add_symbol(1)
@@ -31,7 +30,6 @@ describe Validation do
   end
 
   describe '#add_symbol' do
-    let(:validation) { Validation.new }
     it 'Add a circle to the array in a turn that is odd' do
       validation.user_input = 9
       expect(validation.add_symbol(1)).to eql('o')
